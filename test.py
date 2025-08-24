4c52323cd1093095985ba4f129b3934ea9f0ed59
tanish tirpathi <tanishtirpathi0@gmail.com>
2025-08-24 14:40:45 +0530

    add test file check here

---
+++ test.py
@@ -0,0 +1,8 @@
+from holograph.charts.bar import draw_bar
+from holograph.charts.line import draw_line
+
+print("Testing bar chart:")
+draw_bar([3,4,5,6])
+
+print("\nTesting line chart:")
+draw_line([1,2,3,4,5])
